91140e0753ce736f43f95c6e3d96705592af7acf
ngarciacarrillo <ngarciacarrillo@atanasoff19.danielcastelao.org>
2016-06-06 09:35:28 +0200

    código con formato, variables renombradas y Javadoc

--- src/examenfinalparte1/ExamenFinalParte1.java
+++ src/examenfinalparte1/ExamenFinalParte1.java
@@ -3,17 +3,34 @@ package examenfinalparte1;
 import java.io.IOException;
 
 public class ExamenFinalParte1 {
+    /**
+     * *
+     * @author ngarciacarrillo 
+     * @throws IOException 
+     * @version 6/05/2016-1
+     */
 
     public static void main(String[] args) throws IOException {
-        
+      
+        /**
+         * @params resultado1 Este parámetro es el resultado de sumar los dos
+         * numeros que se pasan a través del objeto
+         */
         Sumar objetoSuma1 = new Sumar(10, 20);
         int resultado1 = objetoSuma1.suma;
         System.out.println("Es: " + resultado1);
-        
+        /**
+         * @params resultado2 Este parámetro es el resultado de sumar el numero 
+         * que se le pasa al objetoSuma2
+         */
         Sumar objetoSuma2 = new Sumar(10);
         int resultado2 = objetoSuma2.suma;
         System.out.println("Es: " + resultado2);
-        
+        /**
+         * @params resultado3 Este parámetro simplemente devuelve menos -1 ya 
+         * que no se le pasa ningún objeto al método y la variable alli definida
+         * es -1
+         */
         Sumar objetoSuma3 = new Sumar();
         int resultado3 = objetoSuma3.suma;
         System.out.println("Es: " + resultado3);
--- src/examenfinalparte1/Sumar.java
+++ src/examenfinalparte1/Sumar.java
@@ -1,21 +1,48 @@
 package examenfinalparte1;
-
+/**
+ * 
+ * @author ngarciacarrillo
+ * @version 6/05/2016-1
+ */
 public class Sumar {
-
+/**
+ * @params suma Este parámetro es el resultado de realizar el métodoSumar según
+ * que constructor se use.
+ */
     int suma;
-
+ /**
+  * @see metodoSumar
+  * @param num1 Parametro que se recoje desde la clase ExamenFinalParte1
+  * @param num2 Parametro que se recoje desde la clase ExamenFinalParte1
+  * 
+  * Este es el constructor de la clase a usar cuando se van a pasar dos numeros 
+  * distintos desde el main.
+  */
     public Sumar(int num1, int num2) {
         suma = metodoSumar(num1, num2);
     }
-
+/**
+ * @see metodoSumar
+ * @param num1 Parametro que se recoje desde la clase ExamenFinalParte1
+ * Este es el constructor de la clase a usar cuando se va a pasar un numero
+  desde el main.
+ */
     public Sumar(int num1) {
         suma = metodoSumar(num1, num1);
     }
-
+/**
+ * @see metodoSumar
+ * Este es el constructor cuando no se recoje ninguna variable
+ */
     public Sumar() {
         suma = -1;
     }
-
+/**
+ * 
+ * @param numSuma1
+ * @param numSuma2
+ * @return devuelve un Int de la suma de los parametros que se pasan
+ */
     public int metodoSumar(int numSuma1, int numSuma2) {
         return numSuma1 + numSuma2;
     }
