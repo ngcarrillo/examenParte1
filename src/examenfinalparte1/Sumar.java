56445c26fc3dfe851db8d29cc3e2e9517328e31d
ngarciacarrillo <ngarciacarrillo@atanasoff19.danielcastelao.org>
2016-06-06 09:13:38 +0200

    código con formato y variables renombradas

--- src/examenfinalparte1/Sumar.java
+++ src/examenfinalparte1/Sumar.java
@@ -5,18 +5,18 @@ public class Sumar {
     int suma;
 
     public Sumar(int num1, int num2) {
-        suma = sumar(num1, num2);
+        suma = metodoSumar(num1, num2);
     }
 
     public Sumar(int num1) {
-        suma = sumar(num1, num1);
+        suma = metodoSumar(num1, num1);
     }
 
     public Sumar() {
         suma = -1;
     }
 
-    public int sumar(int numSuma1, int numSuma2) {
+    public int metodoSumar(int numSuma1, int numSuma2) {
         return numSuma1 + numSuma2;
     }
 }
